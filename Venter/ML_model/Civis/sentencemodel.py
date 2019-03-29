5c88d324a28dd97120017a7be5bc3d940819bfe9
Chintan2108 <chintanmaniyar@gmail.com>
2019-03-29 18:31:17 +0530

    similarity scores for civis

--- Venter/ML_model/Civis/sentencemodel.py
+++ Venter/ML_model/Civis/sentencemodel.py
@@ -1,35 +1,16 @@
-import json
-import os
-import time
-
-import numpy as np
-from gensim.models import KeyedVectors
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.corpus import wordnet as wn
 from nltk.corpus import stopwords
-
-import networkx
+from gensim.models import KeyedVectors
 from networkx.algorithms.components.connected import connected_components
+import os
+import json
+import warnings
+import networkx
+import time
+import numpy as np
 
-
-def toGraph(l):
-    '''
-    It takes in a list of lists and returns a graph object,
-    assigning nodes and edges from each sub-list object
-    '''
-    G = networkx.Graph()
-    for part in l:
-        G.add_nodes_from(part)
-        G.add_edges_from(toEdges(part))
-    return G
-
-def toEdges(l):
-    '''
-    It treats args(1) 'l' as a graph and returns (implicitly) it's edges
-    '''
-    it = iter(l)
-    last = next(it)
-    for current in it:
-        yield last, current
-        last = current
+THRESHOLD_SCORE = 0.4
 
 def toGraph(l):
     '''
@@ -55,7 +36,7 @@ def toEdges(l):
 
 def similarityIndex(s1, s2, wordmodel):
     '''
-    To compare the two sentences for their similarity using the gensim wordmodel
+    To compare the two sentences for their similarity using the gensim wordmodel 
     and return a similarity index
     '''
     if s1 == s2:
@@ -64,22 +45,24 @@ def similarityIndex(s1, s2, wordmodel):
     s1words = s1.split()
     s2words = s2.split()
 
-    s1words = set(s1words)
+    s1words = set(s1words)    
     for word in s1words.copy():
         if word in stopwords.words('english'):
             s1words.remove(word)
+    
     s2words = set(s2words)
     for word in s2words.copy():
         if word in stopwords.words('english'):
             s2words.remove(word)
 
     s1words = list(s1words)
-    s2words = list(s2words)
+    s2words = list(s2words)    
 
     s1set = set(s1words)
     s2set = set(s2words)
 
     vocab = wordmodel.vocab
+    
     if len(s1set & s2set) == 0:
         return 0.0
     for word in s1set.copy():
@@ -88,6 +71,7 @@ def similarityIndex(s1, s2, wordmodel):
     for word in s2set.copy():
         if (word not in vocab):
             s2words.remove(word)
+    
     return wordmodel.n_similarity(s1words, s2words)
 
 
@@ -100,37 +84,32 @@ def categorizer():
 
     st = time.time()
     wordmodelfile = 'E:/Me/IITB/Work/CIVIS/ML Approaches/word embeddings and similarity matrix/GoogleNews-vectors-negative300.bin'
-    wordmodel = KeyedVectors.load_word2vec_format(wordmodelfile, binary = True, limit=500000)
+    wordmodel = KeyedVectors.load_word2vec_format(wordmodelfile, binary = True, limit=200000)
     et = time.time()
     s = 'Word embedding loaded in %f secs.' % (et-st)
     print(s)
     stats.write(s + '\n')
 
     #filepaths
-    responsePath = 'Venter/ML_model/Civis/data/comments/'
-    categoryPath = 'Venter/ML_model/Civis/data/sentences/'
+    responsePath = 'Venter\ML_model\Civis\data\comments'
+    categoryPath = 'Venter\ML_model\Civis\data\sentences'
     responseDomains = os.listdir(responsePath)
     categoryDomains = os.listdir(categoryPath)
-    responseDomains.sort()
-    categoryDomains.sort()
-    print(responseDomains)
-    print(categoryDomains)
+    
     #dictionary for populating the json output
     results = {}
     for responseDomain, categoryDomain in zip(responseDomains, categoryDomains):
         #instantiating the key for the domain
         domain = responseDomain[:-4]
         results[domain] = {}
-        print(responseDomain)
-        print(categoryDomain)
 
         print('Categorizing %s domain...' % domain)
 
-        temp = open(responsePath + responseDomain, 'r', encoding='utf-8-sig')
+        temp = open(os.path.join(responsePath,responseDomain), 'r', encoding='utf-8-sig')
         responses = temp.readlines()
         rows = len(responses)
 
-        temp = open(categoryPath + categoryDomain, 'r', encoding='utf-8-sig')
+        temp = open(os.path.join(categoryPath,categoryDomain), 'r', encoding='utf-8-sig')
         categories = temp.readlines()
         columns = len(categories)
         categories.append('Novel')
@@ -162,14 +141,28 @@ def categorizer():
             results[domain][catName] = []
 
         print('Populating category files...')
-        for score_row, response in zip(similarity_matrix, responses):
+        for score_row,response in zip(similarity_matrix,responses):
             max_sim_index = len(categories)-1
             if np.array(score_row).sum() > 0:
                 max_sim_index = np.array(score_row).argmax()
-            results[domain][categories[max_sim_index]].append(response)
+                temp = {}
+                temp['response'] = response
+                temp['score'] = int((np.array(score_row).max())*100)
+            else:
+                temp = response
+            results[domain][categories[max_sim_index]].append(temp)
         print('Completed.\n')
-        
-        #performing sub-categorization
+
+        #sorting domain wise categorised responses based on scores
+        for domain in results:
+            for category in results[domain]:                                                                                                                                               
+                temp = results[domain][category]
+                if len(temp)==0 or category=='Novel':
+                    continue
+                #print(temp)
+                results[domain][category] = sorted(temp, key=lambda k: k['score'], reverse=True)
+        #newlist = sorted(list_to_be_sorted, key=lambda k: k['name']) --> to sort list of dictionaries
+
         #initializing the matrix with -1 to catch dump/false entries for subcategorization of the novel responses
         no_of_novel_responses = len(results[domain]['Novel'])
         st = time.time()
@@ -178,6 +171,7 @@ def categorizer():
         s = 'Similarity matrix for subcategorization of novel responses for %s domain initialized in %f secs.' % (domain, (et-st))
         print(s)
         stats.write(s + '\n')
+        
 
         #populating the matrix
         row = 0
@@ -190,7 +184,7 @@ def categorizer():
                 similarity_matrix[row][column] = similarityIndex(response1.split('-')[1].lstrip(), response2.split('-')[1].lstrip(), wordmodel)
                 column += 1
             row += 1
-
+        
         setlist = []
         index = 0
         for score_row, response in zip(similarity_matrix, results[domain]['Novel']):
@@ -200,8 +194,10 @@ def categorizer():
             if set([response, results[domain]['Novel'][max_sim_index]]) not in setlist:
                 setlist.append([response, results[domain]['Novel'][max_sim_index]])
             index += 1
+        
         G = toGraph(setlist)
         setlist = list(connected_components(G))
+        
         novel_sub_categories = {}
         index = 0
         for category in setlist:
