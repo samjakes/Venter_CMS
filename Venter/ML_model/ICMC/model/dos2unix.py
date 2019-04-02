e2a19fb992087a302d83dc4b74ee6d7f8d3fe89e
Chintan2108 <chintanmaniyar@gmail.com>
2019-04-02 18:33:53 +0530

    added fix for pickle encoding

---
+++ Venter/ML_model/ICMC/model/dos2unix.py
@@ -0,0 +1,30 @@
+class Dos2Unix:
+    '''
+    STRING opcode argument must be quoted | Hack
+    '''
+    
+
+    def unixencode():
+        '''
+        This function encodes the pickles for dos across unix
+        '''
+        original1 = "Venter\ML_model\ICMC\dataset\dataset_mcgm_clean\word_index_map_mcgm.pickle"
+        destination1 = "Venter\ML_model\ICMC\dataset\dataset_mcgm_clean\word_index_map_mcgm_.pickle"
+        original2 = "Venter\ML_model\ICMC\dataset\dataset_mcgm_clean\word_vectors_mcgm.pickle"
+        destination2 = "Venter\ML_model\ICMC\dataset\dataset_mcgm_clean\word_vectors_mcgm_.pickle"
+
+        content = ''
+        outsize = 0
+        with open(original1, 'rb') as infile:
+            content = infile.read()
+        with open(destination1, 'wb') as output:
+            for line in content.splitlines():
+                outsize += len(line) + 1
+                output.write(line + str.encode('\n'))
+
+        with open(original2, 'rb') as infile:
+            content = infile.read()
+        with open(destination2, 'wb') as output:
+            for line in content.splitlines():
+                outsize += len(line) + 1
+                output.write(line + str.encode('\n'))
