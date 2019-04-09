3bc683232950006de78e027943387f0281057bbe
Preetam Ozarde <preetamozarde3@gmail.com>
2019-04-09 17:27:54 +0530

    Add Logging in settings.py

--- Backend/settings.py
+++ Backend/settings.py
@@ -153,3 +153,19 @@ MAX_UPLOAD_SIZE = "5242880"
 
 FILE_UPLOAD_TYPE = 'csv'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
