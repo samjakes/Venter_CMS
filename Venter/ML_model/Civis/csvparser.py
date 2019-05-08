59039422f40a73f6debcea5c714a86edbe227b27
samjakes <36800087+samjakes@users.noreply.github.com>
2019-05-08 15:28:38 +0530

    Update csvparser.py
    - Changed the slicing logic of the headers 
        from
        `headers = df.keys()[1:]`
        to
        `headers = df.keys()[2:]`
    
        The change in the slicing index fixed the issues with the parsing as tested on Adwait's local machine
    
    - Fixed the path for the output files to use relative paths and os.join instead of hardcoded paths
    
    - Added the corresponding file.close() operation to the file.open operation for each output file being generated

--- Venter/ML_model/Civis/csvparser.py
+++ Venter/ML_model/Civis/csvparser.py
@@ -7,6 +7,9 @@ Created on Mon Feb 25 17:15:22 2019
 
 import pandas as pd
 import re
+import os
+
+from Backend.settings import BASE_DIR
 
 def parse(filepath):
     '''
@@ -17,15 +20,16 @@ def parse(filepath):
     xls = pd.ExcelFile(filepath)
     df = pd.read_excel(xls, 'Form responses 1', header=[0,1])
 
-    headers = df.keys()[1:]
+    headers = df.keys()[2:]
     headers = headers[:len(headers)-3]
     for h in headers[1::2]:
         filename = str(h).split(',')[0].split('\'')[1] + '.txt'
 
-        file = open('Venter/ML_model/Civis/data/comments/' + filename, 'w', encoding='utf-8')
+        file = open(os.path.join(BASE_DIR, 'Venter/ML_model/Civis/data/comments/') + filename, 'w', encoding='utf-8')
         index = 1
         print("Parsing " + filename + '...')
         for sentence in df[h]:
             if type(sentence) == str:
                 file.write(str(index) + '- ' + sentence.lstrip().replace('\n', ' ') + '\n')
                 index += 1
+        file.close()
